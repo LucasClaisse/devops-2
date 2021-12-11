f304d7e147aee0ff612f64000dc46b957b7d6f81
Lucas <lucas.claisse@epitech.eu>
2021-12-11 01:00:09 +0100

    print uri

--- backend/app/dist/database/database.js
+++ backend/app/dist/database/database.js
@@ -23,6 +23,8 @@ exports.connect = () => {
     const host = process.env.MONGO_HOST || "localhost";
     const port = process.env.MONGO_PORT || 27017;
     const uri = `mongodb://${username}:${password}@${host}:${port}/${dbname}?authSource=admin`;
+
+    console.log(uri)
     if (database) {
         return;
     }
